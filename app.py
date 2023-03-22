ed5cf9fd6eb8d40fc5f0de4212cbaf1ad3d4c0eb
ssddi456 <torrent88410@gmail.com>
2023-03-22 17:26:17 +0800

    update

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import json
 import gradio as gr
 
-def greet(name: list):
-    return "Hello " + json.dumps(name) + "!"
+def greet(name: str, num: int):
+    return "Hello " + json.dumps(name) + "! " + str(num)
 
-namelist = gr.List(None, label="Name")
-iface = gr.Interface(fn=greet, inputs=namelist, outputs="text")
-iface.launch()
+with gr.Blocks() as testUI:
+    with gr.Accordion('test accordion'):
+        num = gr.Slider(label="Name")
+        name = gr.Textbox("name", label="input")
 
-# gradio app track current running task process
-process = iface
+    button = gr.Button("Submit")
+    button.click(greet, inputs=[name, num], outputs=gr.Textbox("name", label="output")
+)
+testUI.launch()
+
+# namelist = gr.List(None, label="Name")
+# iface = gr.Interface(fn=greet, inputs=namelist, outputs="text")
+# iface.launch();
+# # gradio app track current running task process
+# process = iface
